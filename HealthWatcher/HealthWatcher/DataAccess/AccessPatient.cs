d752186252d335a94f6f64912fd4e88c4e06a67c
Zeinstra.Victor@gmail.com <Zeinstra.Victor@gmail.com@9f3e1efb-092a-3b58-38f4-b08559ec910c>
2011-11-23 11:24:53 +0000

    adding svn

---
+++ HealthWatcher/HealthWatcher/DataAccess/AccessPatient.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+
+namespace HealthWatcher.DataAccess
+{
+    class AccessPatient
+    {
+        #region attr
+        private ServRefPatient.ServicePatientClient _spc;
+
+        public ServRefPatient.ServicePatientClient Spc
+        {
+            get { return _spc; }
+            set { _spc = value; }
+        }
+        
+        
+        #endregion
+
+        #region ctor
+        public AccessPatient()
+        {
+            Spc = new ServRefPatient.ServicePatientClient();
+        }
+        #endregion
+
+        #region meth
+        #region convert
+        public Byte[] BufferFromImage(BitmapImage imageSource)
+        {
+            Stream stream = imageSource.StreamSource;
+            Byte[] buffer = null;
+            if (stream != null && stream.Length > 0)
+            {
+                using (BinaryReader br = new BinaryReader(stream))
+                {
+                    buffer = br.ReadBytes((Int32)stream.Length);
+                }
+            }
+
+            return buffer;
+        }
+
+        private ServRefPatient.Observation convertObs(Model.Observation obs)
+        {
+            ServRefPatient.Observation newObs = new ServRefPatient.Observation();
+
+            newObs.BloodPressure = obs.BloodPressure;
+            newObs.Weight = obs.Weight;
+            newObs.Comment = obs.Comment;
+            newObs.Date = obs.Date;
+            if (obs.Pictures != null)
+            {
+                for (int i = 0; i < obs.Pictures.Count(); i++)
+                {
+                    newObs.Pictures[i] = BufferFromImage((BitmapImage) obs.Pictures[i].Source);
+                }
+            }
+            if (obs.Prescription != null)
+            {
+                for (int i = 0; i < obs.Prescription.Count; i++)
+                    newObs.Prescription[i] = obs.Prescription[i];
+            }
+            return newObs;
+        }
+
+        public BitmapImage ImageFromBuffer(Byte[] bytes)
+        {
+            MemoryStream stream = new MemoryStream(bytes);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = stream;
+            image.DecodePixelHeight = 120;
+            image.DecodePixelWidth = 120;
+            image.EndInit();
+            return image;
+        }
+
+        private Model.Observation convertObs(ServRefPatient.Observation obs)
+        {
+            Model.Observation newObs = new Model.Observation();
+
+            newObs.BloodPressure = obs.BloodPressure;
+            newObs.Weight = obs.Weight;
+            newObs.Comment = obs.Comment;
+            newObs.Date = obs.Date;
+            newObs.Pictures = new List<Image>();
+            if (obs.Pictures != null)
+            {
+                for (int i = 0; i < obs.Pictures.Count(); i++)
+                {
+                    Image img = new Image();
+                    img.Source = ImageFromBuffer(obs.Pictures[i]);
+                    newObs.Pictures.Add(img);
+                }
+            }
+            newObs.Prescription = new List<string>();
+            if (obs.Prescription != null)
+            {
+                for (int i = 0; i < obs.Prescription.Count(); i++)
+                {
+                    newObs.Prescription.Add(obs.Prescription[i]);
+                }
+            }
+
+            return newObs;
+        }
+
+        private ServRefPatient.Patient convertPat(Model.Patient pat)
+        {
+            ServRefPatient.Patient newPat = new ServRefPatient.Patient();
+
+            newPat.Birthday = pat.Birthday;
+            newPat.Firstname = pat.Firstname;
+            newPat.Name = pat.Name;
+            if (pat.Observations != null)
+            {
+                int i = 0;
+                foreach (Model.Observation observ in pat.Observations)
+                {
+                    newPat.Observations[i++] = convertObs(observ);
+                }
+            }
+            newPat.Id = pat.Id;
+
+            return newPat;
+        }
+
+        private Model.Patient convertPat(ServRefPatient.Patient pat)
+        {
+            Model.Patient newPat = new Model.Patient();
+
+            newPat.Birthday = pat.Birthday;
+            newPat.Firstname = pat.Firstname;
+            newPat.Name = pat.Name;
+            if (pat.Observations != null)
+            {
+                newPat.Observations = new List<Model.Observation>();
+                foreach (ServRefPatient.Observation observ in pat.Observations)
+                {
+                    newPat.Observations.Add(convertObs(observ));
+                }
+            }
+            newPat.Id = pat.Id;
+
+            return newPat;
+        }
+        #endregion convert
+
+        public List<Model.Patient> GetListPatient()
+        {
+            List<Model.Patient> newList = new List<Model.Patient>();
+
+            foreach (ServRefPatient.Patient patient in Spc.GetListPatient())
+            {
+                newList.Add(convertPat(patient));
+            }
+            Spc.GetListPatient();
+
+            return newList; 
+        }
+
+        public Model.Patient GetPatient(int id)
+        {
+            return convertPat(Spc.GetPatient(id));
+        }
+
+        public bool AddPatient(Model.Patient pat)
+        {
+            return Spc.AddPatient(convertPat(pat));
+        }
+
+        public bool DeletePatient(int id)
+        {
+            return Spc.DeletePatient(id);
+        }
+
+        #endregion
+    }
+}
---
+++ HealthWatcher/HealthWatcher/DataAccess/AccessUser.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace HealthWatcher.DataAccess
+{
+    class AccessUser
+    {
+        #region attr
+        private ServRefUser.ServiceUserClient _suc;
+
+        public ServRefUser.ServiceUserClient Suc
+        {
+            get { return _suc; }
+            set { _suc = value; }
+        }
+        #endregion
+
+        #region ctor
+        public AccessUser()
+        {
+            Suc = new ServRefUser.ServiceUserClient();
+        }
+        #endregion
+
+        #region meth
+        #region convert
+        public Byte[] BufferFromImage(BitmapImage imageSource)
+        {
+            Stream stream = imageSource.StreamSource;
+            Byte[] buffer = null;
+            if (stream != null && stream.Length > 0)
+            {
+                using (BinaryReader br = new BinaryReader(stream))
+                {
+                    buffer = br.ReadBytes((Int32)stream.Length);
+                }
+            }
+
+            return buffer;
+        }
+
+        private ServRefUser.User convertUser(Model.User user)
+        {
+            ServRefUser.User newUser = new ServRefUser.User();
+
+            newUser.Login = user.Login;
+            newUser.Pwd = user.Pwd;
+            newUser.Name = user.Name;
+            newUser.Firstname = user.Firstname;
+            newUser.Picture = BufferFromImage((BitmapImage) user.Picture.Source);
+            newUser.Role = user.Role;
+            newUser.Connected = user.Connected;
+
+            return newUser;
+        }
+
+        public BitmapImage ImageFromBuffer(Byte[] bytes)
+        {
+            MemoryStream stream = new MemoryStream(bytes);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = stream;
+            image.EndInit();
+            return image;
+        }
+
+        private Model.User convertUser(ServRefUser.User user)
+        {
+            Model.User newUser = new Model.User();
+
+            newUser.Login = user.Login;
+            newUser.Pwd = user.Pwd;
+            newUser.Name = user.Name;
+            newUser.Firstname = user.Firstname;
+            newUser.Picture = new Image();
+            if (user.Picture != null)
+                newUser.Picture.Source = ImageFromBuffer(user.Picture);
+            newUser.Role = user.Role;
+            newUser.Connected = user.Connected;
+
+            return newUser;
+        }
+        #endregion
+
+        public List<Model.User> GetListUser()
+        {
+            List<Model.User> newList = new List<Model.User>();
+
+            foreach (ServRefUser.User user in Suc.GetListUser())
+            {
+                newList.Add(convertUser(user));
+            }
+            
+            return newList;
+        }
+
+        public Model.User GetUser(string login)
+        {
+            foreach (Model.User user in GetListUser())
+            {
+                if (user.Login == login)
+                    return user;
+            }
+            return null;
+        }
+
+        public bool AddUser(Model.User user)
+        {
+            return Suc.AddUser(convertUser(user));
+        }
+
+        public bool DeleteUser(string login)
+        {
+            return Suc.DeleteUser(login);
+        }
+
+        public bool Connect(string login, string pwd)
+        {
+            return Suc.Connect(login, pwd);
+        }
+
+        public void Disconnect(string login)
+        {
+            Suc.Disconnect(login);
+        }
+        #endregion
+    }
+}
